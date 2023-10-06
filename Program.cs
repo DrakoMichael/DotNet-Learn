4a0da3d1f7c166d51659cbc46003f727c6c675ad
Michael Mello <michaelkcms@gmail.com>
2023-10-06 15:34:25 -0300

    att

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DotNet_Learn.Models;
+
+
+
+Console.WriteLine("Começo da execução");
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Michael";
+pessoa1.Idade = 22;
+pessoa1.Apresentar();
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# DotNet-Learn
- DotNet-Learn
